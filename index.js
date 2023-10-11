5c647c7c729ae1cf68e6ab43d93b31a3c47412e6
root <root@Local-Machine.3vytmlnfdwyefhw0caf5jacuzg.bx.internal.cloudapp.net>
2023-10-11 06:11:53 +0000

    1st

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000; // You can change the port to your desired value
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
